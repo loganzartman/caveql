d1c88e9f5f326e0a6dc8f432d11cffc416cf5f2e
Logan <loganzartman@live.com>
2025-06-18 21:13:22 -0700

    cleanup app

--- repl/App.tsx
+++ repl/App.tsx
@@ -2,25 +2,34 @@ import { useState } from "react";
 import { formatTree, parse } from "../src";
 
 export function App() {
-	const [source, setSource] = useState("");
+	const [source, setSource] = useState(() => {
+		try {
+			return atob(window.location.hash.substring(1));
+		} catch {
+			return "";
+		}
+	});
 
 	let parseTree: string;
 	try {
 		parseTree = formatTree(parse(source));
+		history.replaceState(undefined, "", `#${btoa(source)}`);
 	} catch (e) {
 		parseTree = `Error: ${e instanceof Error ? e.message : String(e)}`;
 	}
 
 	return (
 		<div className="flex flex-row w-full h-full gap-4 p-4">
-			<div className="grow shrink-0">
+			<div className="grow w-0">
+				Type query:
 				<textarea
 					className="w-full h-full font-mono border-2"
 					value={source}
 					onChange={(e) => setSource(e.target.value)}
 				/>
 			</div>
-			<div className="grow shrink-0">
+			<div className="grow w-0">
+				<div>Parse tree:</div>
 				<pre>{parseTree}</pre>
 			</div>
 		</div>
