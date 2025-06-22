065bc2f02ddf3c01c5e65b4777c58fd1ffa6ddef
Logan <logan.zartman@utexas.edu>
2025-06-22 04:34:30 -0700

    compile makeresults

--- src/compile.ts
+++ src/compile.ts
@@ -95,7 +95,49 @@ function compileEvalCommand(command: EvalCommandAST): string {
 }
 
 function compileMakeresultsCommand(command: MakeresultsCommandAST): string {
-	throw new Error("not implemented");
+	if (command.data) {
+		let data: unknown[];
+		if (command.format === "json") {
+			data = JSON.parse(command.data.value);
+		} else if (command.format === "csv") {
+			throw new Error("makeresults format=csv not implemented");
+		} else {
+			impossible(command.format);
+		}
+
+		if (!Array.isArray(data)) {
+			throw new Error("makeresults data must be an array");
+		}
+
+		const items = data.map((item) => {
+			if (typeof item !== "object") {
+				throw new Error("makeresults data items must be objects");
+			}
+			return {
+				_raw: JSON.stringify(item),
+				_time: new Date(),
+				...item,
+			};
+		});
+
+		return `
+      function*(records) {
+        yield* records;
+        yield* ${JSON.stringify(items)};
+      }
+    `;
+	}
+
+	const items = Array.from({ length: Number(command.count.value) }, () => ({
+		_time: new Date(),
+	}));
+
+	return `
+    function*(records) {
+      yield* records;
+      yield* ${JSON.stringify(items)};
+    }
+  `;
 }
 
 function compileWhereCommand(command: WhereCommandAST): string {
