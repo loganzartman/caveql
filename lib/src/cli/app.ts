0a81b4feca3f91ed72bbcfee6dbeffe598a6eae3
Logan <logan.zartman@utexas.edu>
2025-09-01 00:20:11 -0700

    fixup remove test cmd

--- lib/src/cli/app.ts
+++ lib/src/cli/app.ts
@@ -1,11 +1,9 @@
 import { buildApplication, buildRouteMap } from "@stricli/core";
 import { execCommand } from "./commands/exec";
-import { testCommand } from "./commands/test";
 
 export const root = buildRouteMap({
   routes: {
     exec: execCommand,
-    test: testCommand,
   },
   defaultCommand: "exec",
   docs: {
