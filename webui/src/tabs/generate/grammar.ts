5e909828c5ec8430d350457c9c1c520d4536c1cb
Logan <logan.zartman@utexas.edu>
2025-10-19 16:30:59 -0700

    planning

--- webui/src/tabs/generate/grammar.ts
+++ webui/src/tabs/generate/grammar.ts
@@ -1,26 +1,35 @@
 export const grammarGBNF = String.raw`
-root ::= pipeline
-pipeline ::= "|" ws command ("\n" "|" ws command)*
-command ::= rex-command | search-command | stats-command | sort-command
+root ::= query
+query ::= ((("|" ws*)? "search" ws+)? search-expression?)? (ws* "|" ws* command ws*)*
 
-rex-command ::= "rex" ws "field" ws? "=" ws? string ws quoted-string
+command ::= search-command | where-command | rex-command | stats-command | sort-command
+
+rex-command ::= "rex" ws+ "field" ws* "=" ws* string ws quoted-string
 
 search-command ::= "search" ws search-expression
-search-expression ::= search-term (ws search-term)*
-search-term ::= search-comparison
-search-comparison ::= string ws? search-comparator ws? value
+search-expression ::= search-term (ws+ search-term)*
+search-term ::= search-comparison | value | "*"
+search-comparison ::= string ws* search-comparator ws* value
 search-comparator ::= "=" | "!=" | "<" | "<=" | ">" | ">="
 
-sort-command ::= "sort" ws sort-term (ws? "," ws? sort-term)*
-sort-term ::= ("-" | "+")? ws? string
+sort-command ::= "sort" ws+ sort-term (ws* "," ws* sort-term)*
+sort-term ::= ("-" | "+")? ws* string
 
-stats-command ::= "stats" ws stats-term ((ws | ws? "," ws?) stats-term)*
-stats-term ::= "count"
+stats-command ::= "stats" ws+ stats-term ((ws+ | ws* "," ws*) stats-term)*
+stats-term ::= stats-function-call
+stats-function-call ::= stats-function (ws* "(" ws* (string | "*") ws* ")")?
+stats-function ::= "count" | "sum" | "avg" | "min" | "max" | "median"
+
+where-command ::= "where" ws+ where-expression
+where-expression ::= where-term | where-binary-operation
+where-binary-operation ::= where-term ws+ where-binary-operator ws+ where-expression
+where-binary-operator ::= "and" | "or" | "=" | "==" | "!=" | "<" | "<=" | ">" | ">="
+where-term ::= value
 
 value ::= string | number
 string ::= bare-string | quoted-string
 bare-string ::= [a-zA-Z0-9_\-$.]+
 quoted-string ::= "\"" ([^"\\] | "\\\"")* "\""
 number ::= "-"? [0-9]+ ("." [0-9]+)?
-ws ::= [ ]
+ws ::= [ \t\n\r]
 `;
